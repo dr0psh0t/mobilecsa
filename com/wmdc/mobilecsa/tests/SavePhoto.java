a9a07e39d643f915c2d459226e3c06231a4e934c
dr0psh0t <xxrequiemofsoulsxx@gmail.com>
2020-06-15 10:32:19 +0800

    commit june 15

---
+++ com/wmdc/mobilecsa/tests/SavePhoto.java
@@ -0,0 +1,79 @@
+package wmdc.mobilecsa.tests;
+
+import org.json.JSONObject;
+import wmdc.mobilecsa.utils.Utils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet("/savephoto")
+
+@MultipartConfig(fileSizeThreshold=1024*1024*6, // 5MB
+        maxFileSize=1024*1024*3,      // 3MB
+        maxRequestSize=1024*1024*50)   // 50MB
+
+public class SavePhoto extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        JSONObject resJson = new JSONObject();
+
+        Part wophoto = request.getPart("wophoto");
+
+        if (wophoto == null) {
+            Utils.printJsonException(resJson, "Workorder photo null.", out);
+            return;
+        }
+
+        if (wophoto.getSize() < 1) {
+            Utils.printJsonException(resJson, "Workorder photo invalid size.", out);
+            return;
+        }
+
+        InputStream wophotoStream = wophoto.getInputStream();
+
+        Connection conn = null;
+        PreparedStatement prepStmt = null;
+
+        try {
+            Utils.databaseForName(getServletContext());
+            conn = Utils.getConnection(getServletContext());
+
+            prepStmt = conn.prepareStatement("INSERT INTO qc_photos (photo, content_type) VALUES (?, ?)");
+            prepStmt.setBlob(1, wophotoStream);
+            prepStmt.setString(2, wophoto.getContentType());
+            prepStmt.execute();
+
+            Utils.printSuccessJson(resJson, "OK", out);
+
+        } catch (ClassNotFoundException | SQLException sqe) {
+            Utils.printJsonException(resJson, sqe.toString(), out);
+
+        } catch (Exception e) {
+            Utils.printJsonException(resJson, e.toString(), out);
+
+        } finally {
+            Utils.closeDBResource(conn, prepStmt, null, getServletContext());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
